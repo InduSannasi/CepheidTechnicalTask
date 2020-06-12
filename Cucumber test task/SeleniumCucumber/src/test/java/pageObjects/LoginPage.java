160eb0103254d6e9d8fa27a0661521daa6445afd
InduSannasi <66753135+InduSannasi@users.noreply.github.com>
2020-06-12 08:17:55 +0200

    Add files via upload

---
+++ Cucumber test task/SeleniumCucumber/src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,49 @@
+package pageObjects;
+
+import com.paulhammant.ngwebdriver.ByAngularModel;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public WebDriver driver;
+
+    public LoginPage(WebDriver rdriver)
+    {
+        driver = rdriver;
+        PageFactory.initElements(rdriver,this);
+    }
+    @ByAngularModel.FindBy(model = "user.name")
+    WebElement username;
+
+    @ByAngularModel.FindBy(model = "user.password")
+    WebElement password;
+
+    @FindBy(css="button[type='submit']")
+    WebElement login;
+
+    @FindBy(css="p[ng-click='logout()']")
+    WebElement logout;
+
+
+
+    public void setUsername(String name){
+        //username.clear();
+        username.sendKeys(name);
+    }
+
+    public void setPassword(String word){
+        password.clear();
+        password.sendKeys(word);
+    }
+
+    public void clickLogin(){
+        login.click();
+    }
+
+    public void clickLogout(){
+        logout.click();
+    }
+
+}
---
+++ Cucumber test task/SeleniumCucumber/src/test/java/pageObjects/UpdatePage.java
@@ -0,0 +1,63 @@
+package pageObjects;
+
+import com.paulhammant.ngwebdriver.ByAngularModel;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UpdatePage {
+    public WebDriver driver;
+
+    public UpdatePage(WebDriver rdriver)
+    {
+        driver = rdriver;
+        PageFactory.initElements(rdriver,this);
+    }
+
+    @ByAngularModel.FindBy(model = "selectedEmployee.firstName")
+    WebElement firstName;
+
+    @ByAngularModel.FindBy(model = "selectedEmployee.lastName")
+    WebElement lastName;
+
+    @ByAngularModel.FindBy(model = "selectedEmployee.startDate")
+    WebElement startDate;
+
+    @ByAngularModel.FindBy(model = "selectedEmployee.email")
+    WebElement email;
+
+    @FindBy(xpath="//button[text()=\"Add\"]")
+    WebElement addBtn;
+
+    @FindBy(xpath="//button[text()=\"Update\"]")
+    WebElement updateBtn;
+
+    public void setFirstName(String name){
+        firstName.clear();
+        firstName.sendKeys(name);
+    }
+
+    public void setLastName(String word){
+        lastName.clear();
+        lastName.sendKeys(word);
+    }
+
+    public void setStartDate(String date){
+        startDate.clear();
+        startDate.sendKeys(date);
+    }
+    public void setEmail(String value){
+        email.clear();
+        email.sendKeys(value);
+    }
+
+    public void clickAddBtn(){
+        addBtn.click();
+    }
+
+    public void clickUpdateBtn(){
+        updateBtn.click();
+    }
+
+}
